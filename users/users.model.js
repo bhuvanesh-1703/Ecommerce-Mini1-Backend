4b2e2edd9b3cd6d434b24718c67baf074bc3de62
bhuvanesh-1703 <bhuvaneshmahi2003@gmail.com>
2026-03-21 23:18:12 +0530

    feat: implement Mongoose user model and update the start script in package.json.

--- users/users.model.js
+++ users/users.model.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, unique: true },
     email: { type: String, unique: true, required: true },
     phonenumber: { type: Number },
-    role: { type: String, default: "user" },
+    role: { type: String, default: "admin" },
     password: { type: String, required: true },
     status: { type: String, default: "Active" },
     role: { type: String }
